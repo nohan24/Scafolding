f0a6e4ff55f5b2fecf282bd526c403a8e50dbef1
jonathandry32 <bayronkevin1@gmail.com>
2024-02-18 17:21:01 +0300

    generation view fonction

--- Generation.java
+++ Generation.java
@@ -102,7 +102,84 @@ public class Generation {
         } 
 
     }
+    
+    private static void generateViewCreate(String modelName, String packageName){
 
+        Path path = Paths.get("view/create.tpl");
+        String namespace = getProjectName().concat(".Views");
+        if(packageName != null)namespace.concat(".".concat(packageName));
+        String fileName = packageName != null ? "Views/".concat(packageName).concat("/").concat(capitalize(modelName)).replace('.', '/') : "Views/".concat(capitalize(modelName));
+        try {
+            if(packageName != null){
+                File dir = new File("Views/".concat(packageName).replace('.', '/'));
+                dir.mkdirs();
+            }
+            PrintWriter writer = new PrintWriter(new FileWriter("insert".concat(fileName).concat(".cshtml")));
+            String champs = "";
+
+            List<Column> columns = database.getTableColumns(table);
+            for(Column c : columns){
+                champs += "\n".concat("<div class=\"form-group\">").concat("\t\t");
+                champs += "\n".concat("<label asp-for=\"").concat(c.getName()).concat("\" class=\"control-label\">").concat(c.getName()).concat(" : \t\t\t");
+                champs += "\n".concat("<input asp-for=\"").concat(c.getName()).concat("\" class=\"form-control\" />").concat("\t\t\t");
+                champs += "\n".concat("<span asp-validation-for=\"").concat(c.getName()).concat("\" class=\"text-danger\"></span>").concat("\t\t\t");
+                champs += "\n".concat("</div>").concat("\t\t");
+            }
+
+            String viewFile = Files.readString(path);
+            viewFile = viewFile.replace("#modelName#", modelName);
+            viewFile = viewFile.replace("#champs#", champs);
+            writer.println(viewFile);
+            writer.close();
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } 
+
+    }
+    
+    private static void generateViewUpdate(String modelName, String packageName){
+
+        Path path = Paths.get("view/update.tpl");
+        String namespace = getProjectName().concat(".Views");
+        if(packageName != null)namespace.concat(".".concat(packageName));
+        String fileName = packageName != null ? "Views/".concat(packageName).concat("/").concat(capitalize(modelName)).replace('.', '/') : "Views/".concat(capitalize(modelName));
+        try {
+            if(packageName != null){
+                File dir = new File("Views/".concat(packageName).replace('.', '/'));
+                dir.mkdirs();
+            }
+            PrintWriter writer = new PrintWriter(new FileWriter("update".concat(fileName).concat(".cshtml")));
+            String champs = "";
+
+            List<Column> columns = database.getTableColumns(table);
+            for(Column c : columns){
+                if (!c.isPrimaryKey()) {
+                    champs += "\n".concat("<div class=\"form-group\">").concat("\t\t");
+                    champs += "\n".concat("<label asp-for=\"").concat(c.getName()).concat("\" class=\"control-label\">").concat(c.getName()).concat(" : \t\t\t");
+                    champs += "\n".concat("<input asp-for=\"").concat(c.getName()).concat("\" class=\"form-control\" />").concat("\t\t\t");
+                    champs += "\n".concat("<span asp-validation-for=\"").concat(c.getName()).concat("\" class=\"text-danger\"></span>").concat("\t\t\t");
+                    champs += "\n".concat("</div>").concat("\t\t");
+                }else{
+                    champs += "\n".concat("<input type=\"hidden\" asp-for=\"Id\" />").concat("\t\t");
+                }
+            }
+
+            String viewFile = Files.readString(path);
+            viewFile = viewFile.replace("#modelName#", modelName);
+            viewFile = viewFile.replace("#champs#", champs);
+            writer.println(viewFile);
+            writer.close();
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } 
+
+    }
     private static void generateController(){
 
     }
